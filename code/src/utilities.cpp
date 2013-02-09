7c9ea56fe98fba8f7e6da1ab8bd75073968e30d0
Colin Chang <colinichang@berkeley.edu>
2013-02-09 00:01:54 -0800

    utilities: added bounding box utility function

--- code/src/utilities.cpp
+++ code/src/utilities.cpp
@@ -14,3 +14,14 @@ void wait_once(){
 	string s;
 	getline(cin, s);
 }
+
+/*
+ makeBoundingBox takes a vector of ofRectangles and returns an ofRectangles that surrounds all of those in the vector
+ */
+ofRectangle makeBoundingBox(std::vector<ofRectangle> bounds){
+	ofRectangle boundingBox = ofRectangle();
+	for (std::vector<ofRectangle>::iterator it = bounds.begin(); it != bounds.end(); ++it){
+		boundingBox.growToInclude(*it);
+	}
+	return boundingBox;
+}
--- code/src/utilities.h
+++ code/src/utilities.h
@@ -1,7 +1,8 @@
 #include <math.h>
 #include"ofxOpenCv.h"
-
 #pragma once
 
 ofVec3f centerOf(CvRect rect);
 void wait_once();
+
+ofRectangle makeBoundingBox(std::vector<ofRectangle> bounds);
