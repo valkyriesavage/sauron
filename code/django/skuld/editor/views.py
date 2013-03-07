5b82fc9a5de211e055ea1aad9808506e515afa62
Valkyrie Savage <valkyrie@eecs.berkeley.edu>
2013-03-07 14:39:13 -0800

    removing now-unused codepaths
      * don't need that sloppy stuff laying around

--- code/django/skuld/editor/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-
-def design(request):
-  return render_to_response('design.html', {
-                              'something' : 'yaya data',
-                              }, context_instance = RequestContext(request))
--- code/openSCAD/test_sphere.scad
+++
@@ -1,7 +0,0 @@
-translate([0,0,1]) {
-	union() {
-		translate([0,0,25])
-			sphere(25);
-		cube(size=[50,50,2], center=true);
-	}
-}
